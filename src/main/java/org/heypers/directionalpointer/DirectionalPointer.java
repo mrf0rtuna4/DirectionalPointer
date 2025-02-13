a5f26aa0eae3a19801d1072b27acb78312a7f1f0
mrf0rtuna4 <majned90@gmail.com>
2025-02-13 21:34:11 +0300

    feat(mod): init

--- src/main/java/org/heypers/directionalpointer/DirectionalPointer.java
+++ src/main/java/org/heypers/directionalpointer/DirectionalPointer.java
@@ -1,4 +1,13 @@
 package org.heypers.directionalpointer;
 
-public class DirectionalPointer {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DirectionalPointer implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("DirectionalPointer мод загружен!");
+    }
 }
--- src/main/java/org/heypers/directionalpointer/client/DirectionalPointerClient.java
+++ src/main/java/org/heypers/directionalpointer/client/DirectionalPointerClient.java
@@ -1,4 +1,13 @@
 package org.heypers.directionalpointer.client;
 
-public class DirectionalPointerClient {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DirectionalPointerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("DirectionalPointerClient инициализирован!");
+    }
 }
